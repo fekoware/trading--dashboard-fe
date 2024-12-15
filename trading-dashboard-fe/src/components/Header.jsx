b145bd34f93ab5614628fe4cda1f73b4e2c2be9e
Mayowa Olufeko <mayfeko@gmail.com>
2024-12-15 13:05:17 +0000

    testing components

---
+++ trading-dashboard-fe/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+export const Header = () => {
+    return (
+        <Link to='/accounts'>
+            <button> Account </button>
+        </Link>
+
+    )
+}
